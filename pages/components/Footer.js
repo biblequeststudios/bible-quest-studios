14e437a642d1336c75bbbfe3624c8197a11b99dc
biblequeststudios <FaithForgeStudios@gmail.com>
2025-06-20 15:30:59 +0300

    Delete pages/components directory

--- pages/components/Footer.js
+++
@@ -1,7 +0,0 @@
-export default function Footer() {
-  return (
-    <footer className="bg-gray-100 py-6 mt-8 border-t text-center text-gray-500 text-sm">
-      &copy; {new Date().getFullYear()} Bible Quest Studios
-    </footer>
-  );
-}
