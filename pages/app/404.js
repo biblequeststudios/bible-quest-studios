dff67d9c1b7cb05c453d0c0508b6056fcb646b65
biblequeststudios <FaithForgeStudios@gmail.com>
2025-06-20 12:37:05 +0300

    Create 404.js
    Replace all pages.js files at root

---
+++ pages/app/404.js
@@ -0,0 +1,13 @@
+import Layout from "../components/Layout";
+
+export default function Custom404() {
+  return (
+    <Layout>
+      <div className="text-center py-16">
+        <h1 className="text-4xl font-bold mb-4">404 - Page Not Found</h1>
+        <p className="text-lg mb-6">Sorry, we couldn't find what you were looking for.</p>
+        <a href="/" className="text-blue-600 hover:underline">Go back home</a>
+      </div>
+    </Layout>
+  );
+}
