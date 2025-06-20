a0c1882b0cd57c93a8b34c753ef6c52409b01bba
biblequeststudios <FaithForgeStudios@gmail.com>
2025-06-20 15:25:29 +0300

    Create Footer.js

---
+++ components/Footer.js
@@ -0,0 +1,7 @@
+export default function Footer() {
+  return (
+    <footer className="bg-gray-100 py-6 mt-8 border-t text-center text-gray-500 text-sm">
+      &copy; {new Date().getFullYear()} Bible Quest Studios
+    </footer>
+  );
+}
