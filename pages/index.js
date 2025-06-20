89520b12aea1a41609ae30b9071c7fd2d22c37ef
biblequeststudios <FaithForgeStudios@gmail.com>
2025-06-20 12:29:52 +0300

    Update index.js
    update index.js file at root

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,15 @@
+import Layout from "../components/Layout";
+import QuizCard from "../components/QuizCard";
+
 export default function Home() {
-  return <div>Hello Bible Quest!</div>
+  return (
+    <Layout>
+      <h2 className="text-xl font-bold mb-6">Welcome to Bible Quest!</h2>
+      <QuizCard
+        question="Who led the Israelites out of Egypt?"
+        answers={["Abraham", "Moses", "David", "Paul"]}
+        correct="Moses"
+      />
+    </Layout>
+  );
 }
